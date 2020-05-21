1031507d0bbf05a528ee7c444b0c55206f742665
Shlomo Carmi <shlomow6@gmail.com>
2020-05-21 09:36:32 +0300

    removed redundency

--- src/pages/login page/LoginPage.js
+++ src/pages/login page/LoginPage.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Dictionary, langs, LangBtn } from '../../Dictionary'
+import { Dictionary, LangBtn } from '../../Dictionary'
 import './LoginPage.css';
 import { Link } from 'react-router-dom';
 import logo from '../../images/logo.png';
