e969f4f874ec306939b92c8f858f63464374f7e3
Shlomo Carmi <shlomow6@gmail.com>
2020-05-22 10:44:40 +0300

    removed redundencies

---
+++ src/components.js
@@ -0,0 +1,9 @@
+import logo from '../../images/logo.png';
+export const navBar = () => {
+
+    return (
+      <div id ="navbar">
+        <img id= "smallLogo" src = {logo} alt = "logo"></img>
+           <LangBtn />
+</div>
+)}  
