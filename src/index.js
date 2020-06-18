f0ac193137b79519ed5d9db788e3b1dccb26e8ed
Adiel Tsayag <adielts054@gmail.com>
2020-06-18 14:09:35 +0300

    login - session update

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import './index.css';
 import $ from 'jquery';
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import LoginPage from './pages/login page/LoginPage';
+import { LoginComponent } from './pages/login page/LoginPage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainUserPage from './pages/main user page/MainUserPage';
 import WomanPage from './pages/woman page/WomanPage';
 import Category from './pages/category/Category';
+import { auth } from './config/Firebase';
 
-ReactDOM.render(
 
-    <Router>
-        <Route exact path="/" component={LoginPage} />
-        <Route path="/MainUserPage" component={MainUserPage} />
-        <Route path="/WomanPage" component={WomanPage} />
-        <Route path="/Category" component={Category} />
-    </Router>, document.getElementById('root')
-);
+{/* <Router>
+    <Route exact path="/" component={LoginPage} />
+    <Route path="/MainUserPage" component={MainUserPage} />
+    <Route path="/WomanPage" component={WomanPage} />
+    <Route path="/Category" component={Category} />
+</Router>, document.getElementById('root')
+); */}
 
 $("document").ready(function () {
-
 });
+
+ReactDOM.render(
+    <LoginComponent />, document.getElementById('root')
+)
--- src/pages/main user page/mainUserPage.js
+++ src/pages/main user page/mainUserPage.js
@@ -4,33 +4,32 @@ import React, { Component } from 'react';
 import { NavBar, PictursCarousel, DisplayModal, BottomBar, AfterMessage } from '../../Components.js';
 import { FeedbackButton } from '../../forms/Forms';
 import { getWoman, WomenCard } from '../woman page/WomanPage';
-
-
+import { auth } from '../../config/Firebase';
 
 
 class MainUserPage extends Component {
     render() {
         return (
             <div id="mainUPWrapper" className="wrapper">
-                
-             
+
+
                 <NavBar />
 
-                
+
                 <FeedbackButton />
                 <PictursCarousel />
-                
+
                 {/* <BottomBar/> */}
-                 {/* <DisplayModal link='https://he.wikipedia.org/wiki/%D7%A0%D7%A2%D7%9E%D7%AA' details='Wikipedia'/> */}
+                {/* <DisplayModal link='https://he.wikipedia.org/wiki/%D7%A0%D7%A2%D7%9E%D7%AA' details='Wikipedia'/> */}
                 {/* <AfterMessage info='this is check!!!' />  */}
             </div>
         );
-        
+
     }
 }
 export default MainUserPage
-$(document).ready( () => {
-    
-        //  getWoman("שלמה כרמי");
-    });
+$(document).ready(() => {
+
+    //  getWoman("שלמה כרמי");
+});
 
