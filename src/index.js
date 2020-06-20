31bbc48548576a80fb4757dafc043f671bcce13d
Matan Yamin <you@example.com>
2020-06-20 21:13:43 +0300

    Admin page platform - pilot

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { LoginComponent } from './pages/login page/LoginPage';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import MainUserPage from './pages/main user page/MainUserPage';
 import WomanPage from './pages/woman page/WomanPage';
+import AdminPage from './pages/Admin Page/AdminPage';
 import Category from './pages/category/Category';
 import { auth } from './config/Firebase';
 
